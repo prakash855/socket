1c1ae11e32f0808716f9a55a9f2dc419b6395446
Prakash Agrahari <56213227+prakash855@users.noreply.github.com>
2022-05-04 18:07:12 +0530

    Initial commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+const SideBar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="intro">
+        <div className="text_headings headFive">Stringle</div>
+      </div>
+      <div className="menuTabs"></div>
+      <div className="action-upgrade"></div>
+    </aside>
+  );
+};
+
+export default SideBar;
